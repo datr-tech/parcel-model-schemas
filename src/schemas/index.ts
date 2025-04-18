39294589c8416db283a3bf455c3ec744420b3936
joealdersonstrachan <joealdersonstrachan@strachan.email>
2025-04-18 14:29:31 +0100

    feat: update deps

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,3 +1,4 @@
+import * as commonSchemasModule from '@datr.tech/parcel-model-schemas-common-fields';
 import * as dolomiteSchemasModule from '@datr.tech/parcel-model-schemas-dolomite';
 import * as entitySchemasModule from '@datr.tech/parcel-model-schemas-entity';
 import * as freightSchemasModule from '@datr.tech/parcel-model-schemas-freight';
@@ -5,6 +6,7 @@ import * as granul8SchemasModule from '@datr.tech/parcel-model-schemas-granul8';
 import * as personaSchemasModule from '@datr.tech/parcel-model-schemas-persona';
 import * as procSchemasModule from '@datr.tech/parcel-model-schemas-proc';
 
+export const commonSchemas = commonSchemasModule;
 export const dolomiteSchemas = dolomiteSchemasModule;
 export const entitySchemas = entitySchemasModule;
 export const freightSchemas = freightSchemasModule;
